8b1713f5b949544746cabb79d6867f56308e612f
Adit Gupta <guptaditz@gmail.com>
2026-06-20 07:13:35 -0700

    made speed bar

--- scenes/pigeon.gd
+++ scenes/pigeon.gd
@@ -5,7 +5,15 @@ extends CharacterBody2D
 @export var acceleration: int = 20
 @export var drag: int = 2
 @export var angular_speed: float = PI/36
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var timer: Timer
+
+var input_linear: float
+var input_angular: float
+
+
+
 
 func _physics_process(delta: float) -> void:
 	var input_linear: float = Input.get_axis("Slow Down", "Speed Up")
@@ -16,9 +24,15 @@ func _physics_process(delta: float) -> void:
 	if input_linear > 0:
 		var forward = Vector2.UP.rotated(rotation)
 		velocity = velocity.move_toward(forward * max_speed, acceleration)
+		Global.emit_signal("get_speed", velocity.length())
 	elif velocity.length() < min_speed:
 		velocity = Vector2.UP.rotated(rotation) * min_speed
+		Global.emit_signal("get_speed", velocity.length())
+	elif input_linear == -1:
+		velocity = velocity.move_toward(Vector2.ZERO, drag*2)
+		Global.emit_signal("get_speed", velocity.length())
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, drag)
+		Global.emit_signal("get_speed", velocity.length())
 
 	move_and_slide()
