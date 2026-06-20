beabb39e83293522f358954a335d5043081a1332
ErrorCode0 <me@errorcodezero.dev>
2026-06-19 21:23:50 -0700

    whatever this is

--- scenes/pigeon.gd
+++ scenes/pigeon.gd
@@ -1,26 +1,35 @@
 extends CharacterBody2D
 
+@export var linear_acceleration: int = 2
+@export var angular_speed: float = PI/36
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var angle: float = PI/2;
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input linear_direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction: Vector2 = Vector2.ZERO;
+	var input_linear_direction: float = Input.get_axis("Speed Up", "Slow Down")
+	var angular_direction: float = Input.get_axis("Turn Left", "Turn Right")
+
+	direction.y = input_linear_direction * sin(angular_direction)
+	direction.x = input_linear_direction * cos(angular_direction)
+
+
 	if direction:
-		velocity.x = direction * SPEED
+		velocity += direction;
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, linear_acceleration)
+		velocity.x = move_toward(velocity.x, 0, linear_acceleration)
+
+		
+	if angular_direction:
+		
+		rotate(angular_speed * angular_direction)
+	else:
+		rotate(0)
 
 	move_and_slide()
