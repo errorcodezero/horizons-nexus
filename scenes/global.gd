f1a7909239d8cc5214ce567f96a771038d0f7c9d
Adit Gupta <guptaditz@gmail.com>
2026-06-21 10:06:33 -0700

    made bombing buildings yay

--- scenes/global.gd
+++ scenes/global.gd
@@ -1,3 +1,19 @@
 extends Node
 
 signal get_speed(velocity: float)
+signal score_changed(new_score: int)
+signal city_cleared
+
+var score: int = 0
+var buildings_in_level: int = 0
+var buildings_destroyed_count: int = 0
+
+func register_building() -> void:
+	buildings_in_level += 1
+
+func building_destroyed() -> void:
+	score += 100
+	buildings_destroyed_count += 1
+	score_changed.emit(score)
+	if buildings_destroyed_count >= buildings_in_level:
+		city_cleared.emit()
--- scenes/pigeon/dropping.gd
+++ scenes/pigeon/dropping.gd
@@ -1,21 +1,36 @@
 extends CharacterBody2D
 
-@export_category("Stats")
-@export var damage: float
+@export var damage: float = 1
+@export var blast_radius: float = 40.0
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var fall_length: Timer = $"Fall Length"
-@onready var hitbox: Area2D = $Hitbox
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var fall_timer: Timer = $FallTimer
 
 func _ready() -> void:
-	hitbox.get_child(0).disabled = true
-	
+	fall_timer.timeout.connect(_explode)
+
 func drop() -> void:
-	fall_length.start()
-	animated_sprite_2d.set_frame_and_progress(0, 0)
-	animated_sprite_2d.play("default")
-	await fall_length.timeout
-	animated_sprite_2d.hide()
-	hitbox.get_child(0).disabled = false
-	await get_tree().create_timer(4).timeout
+	animated_sprite.set_frame_and_progress(0, 0)
+	animated_sprite.play("default")
+	fall_timer.start()
+
+func _explode() -> void:
+	animated_sprite.hide()
+
+	var space_state := get_world_2d().direct_space_state
+	var query := PhysicsShapeQueryParameters2D.new()
+	var circle := CircleShape2D.new()
+	circle.radius = blast_radius
+	query.shape = circle
+	query.transform = Transform2D(0, global_position)
+	query.collision_mask = 2
+	query.collide_with_areas = true
+
+	var results := space_state.intersect_shape(query)
+	for result in results:
+		var collider := result.collider as Node
+		if collider and collider.has_method("take_damage"):
+			collider.take_damage(damage)
+
+	await get_tree().create_timer(0.5).timeout
 	queue_free()
